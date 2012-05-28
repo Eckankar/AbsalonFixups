79cc9c8a9461e93f39d2d2426e2004291448ec31
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2012-05-28 10:36:22 +0200

    Durr

--- autocomplete.js
+++ autocomplete.js
@@ -1,8 +1,8 @@
 function $(id) { return document.getElementById(id); }
 
 function enableAutocomplete() {
-    $('#ctl03_Login_username_input').removeAttribute('autocomplete');
-    $('#ctl03_Login_password_input').removeAttribute('autocomplete');
+    $('ctl03_Login_username_input').removeAttribute('autocomplete');
+    $('ctl03_Login_password_input').removeAttribute('autocomplete');
 }
 
 window.addEventListener('DOMContentLoaded', enableAutocomplete, false);
