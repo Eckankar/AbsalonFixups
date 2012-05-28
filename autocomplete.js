6aabf344460e3099f8007bb3620167e06756559c
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2012-05-28 10:31:04 +0200

    Correct autocomplete script

--- autocomplete.js
+++ autocomplete.js
@@ -1,8 +1,8 @@
 function $(id) { return document.getElementById(id); }
 
 function enableAutocomplete() {
-    $('Login_username_input').removeAttribute('autocomplete');
-    $('Login_password_input').removeAttribute('autocomplete');
+    $('#ctl03_Login_username_input').removeAttribute('autocomplete');
+    $('#ctl03_Login_password_input').removeAttribute('autocomplete');
 }
 
 window.addEventListener('DOMContentLoaded', enableAutocomplete, false);
